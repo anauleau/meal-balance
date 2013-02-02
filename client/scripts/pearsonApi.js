6e5bb20be88f549c9b5a6b89f60dffcda1d7cabd
Nathan Houle <ndhoule@gmail.com>
2013-02-02 11:45:44 -0800

    Fixed indentation; missing semicolons; misspelled var name

--- client/scripts/pearsonApi.js
+++ client/scripts/pearsonApi.js
@@ -1,14 +1,16 @@
 var headerSetter = function(xhr) {
   xhr.setRequestHeader("X-Parse-REST-API-Key", "2e02a8b595c1cd7e9a53cc39d1b8ce21");
 };
-var cuisines = "American"
-var dataByCusine = "https://api.pearson.com/kitchen-manager/v1/cuisines/"+ cuisines +".json?apikey=2e02a8b595c1cd7e9a53cc39d1b8ce21"
+
+var cuisines = "American";
+var dataByCuisine = "https://api.pearson.com/kitchen-manager/v1/cuisines/" + cuisines + ".json?apikey=2e02a8b595c1cd7e9a53cc39d1b8ce21";
+
 $.ajax({
   type: "GET",
-  url: dataByCusine,
+  url: dataByCuisine,
   beforeSend: headerSetter,
   contentType: 'application/json',
   success: function(data){
-	console.log(data);
+    console.log(data);
   }
 });
