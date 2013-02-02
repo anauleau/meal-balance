4f37dc482ec892229ec01a0f8c0ee249da15a9f3
Nathan Houle <ndhoule@gmail.com>
2013-02-02 11:45:26 -0800

    Fixed indentation, missing semicolons

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,18 +1,13 @@
 if (Meteor.isClient) {
+  Template.basicForm.events({
+    'click #submit' : function(){
+      var searchFields = ["search", "cuisine", "diet"];
 
-	Template.basicForm.events({
-	'click #submit' : function(){
-
-		var searchFields = ["search", "cuisine", "diet"]
-
-		var searchTerms = {};
-	       //refactor and use reduce instead of for loop
-	  for (var i = 0; i < fields.length; i++){
-	  	searchTerms[fields[i]] = $("#" + fields[i]).val();
-	  }
-
-	}
-
-	});
-
+      var searchTerms = {};
+      //TODO: refactor and use reduce instead of for loop
+      for (var i = 0; i < fields.length; i++){
+        searchTerms[fields[i]] = $("#" + fields[i]).val();
+      }
+    }
+  });
 }
