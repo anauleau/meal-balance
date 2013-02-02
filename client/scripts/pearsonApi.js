3219285ed545a9ddd85cc8b89746d3990f01725a
June Jung <jeongjueun@gmail.com>
2013-02-02 12:07:56 -0800

    WIP : pearsonAPI gets data

--- client/scripts/pearsonApi.js
+++ client/scripts/pearsonApi.js
@@ -1,3 +1,6 @@
-$.getJSON("https://api.pearson.com/kitchen-manager/v1/cuisines/American.json?apikey=2e02a8b595c1cd7e9a53cc39d1b8ce21",function(data){
-  console.log(data);
+$.ajax({
+  type: "GET", 
+  dataType: "jsonp",
+  url: "https://api.pearson.com/kitchen-manager/v1/cuisines/American.json?apikey=2e02a8b595c1cd7e9a53cc39d1b8ce21",
+  success: function(data){console.log(data);}
 });
