d7d2b6442ec3fba78dbb4e8bb380ff81cc4e2b1c
June Jung <jeongjueun@gmail.com>
2013-02-03 11:37:03 -0800

    change the api method

--- client/scripts/pearsonApi.js
+++ client/scripts/pearsonApi.js
@@ -1,13 +1,15 @@
-var getDataByCuisine = function(filter){
+ var getDataByCuisine = function(filter){
   var cuisineAPI = "https://api.pearson.com/kitchen-manager/v1/recipes.json?ingredients-any="+filter.keyWord+"&apikey=3e8c8773334c91e0614872759ec4f303";
   $.ajax({
     type: "GET",
     dataType: "jsonp",
     url: cuisineAPI,
     success: function(data){
-      console.log(data);
-      filterArray = ["it", "should", "be", "user","'s","input"];
-      getResultRecipes(data.results, filterArray);
+      var ingredientFilter = _.map(filter.diet, function(eachDiet){
+        return eachDiet;
+      });
+      ingredientFilter = _.uniq(_.flatten(ingredientFilter));
+      getResultRecipes(data.results, ingredientFilter);
     }
   });
 }
